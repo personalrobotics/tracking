d26bde426078ace45f4b7627cdfe0f8b7fe7c9a6
Kevin Hsu <kevhsu1@uw.edu>
2020-10-13 15:12:03 -0700

    Changes to makefile for Python3

--- makefile
+++ makefile
@@ -3,13 +3,13 @@ SRCS = src/tracking_class.cpp
 
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
-    CFLAGS = -g -Wall -std=c++11 -O3 -Wall -fPIC -shared -std=c++11 `python -m pybind11 --includes` -I /usr/include/python2.7 -I ./
+    CFLAGS = -g -Wall -std=c++11 -O3 -Wall -fPIC -shared -std=c++11 `python3 -m pybind11 --includes` -I /usr/include/python2.7 -I ./
     PROG = src/find_marker.so
 endif
 
 ifeq ($(UNAME_S),Darwin)
     CFLAGS = -g -Wall -std=c++11 -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup `python -m pybind11 --includes` -I ./
-    PROG = src/find_marker`python-config --extension-suffix`
+    PROG = src/find_marker`python3-config --extension-suffix`
 endif
 
 $(PROG):$(SRCS)
--- src/test_settings.py
+++ src/test_settings.py
@@ -7,23 +7,6 @@ import marker_dectection
 import find_marker
 import setting
 
-# max_value = 255
-# max_value_H = 360//2
-# low_H = 0
-# low_S = 0
-# low_V = 0
-# high_H = max_value_H
-# high_S = max_value
-# high_V = max_value
-# window_capture_name = 'Video Capture'
-# window_detection_name = 'Object Detection'
-# low_H_name = 'Low H'
-# low_S_name = 'Low S'
-# low_V_name = 'Low V'
-# high_H_name = 'High H'
-# high_S_name = 'High S'
-# high_V_name = 'High V'
-
 
 rescale = int(sys.argv[1])
 low_x0 = 0
@@ -68,69 +51,22 @@ def dy_trackbar(val):
     low_dy = val
     cv2.setTrackbarPos(dy_name, window_setting_name, low_dy)
 
-
-# def on_low_H_thresh_trackbar(val):
-#     global low_H
-#     global high_H
-#     low_H = val
-#     low_H = min(high_H-1, low_H)
-#     cv.setTrackbarPos(low_H_name, window_detection_name, low_H)
-# def on_high_H_thresh_trackbar(val):
-#     global low_H
-#     global high_H
-#     high_H = val
-#     high_H = max(high_H, low_H+1)
-#     cv.setTrackbarPos(high_H_name, window_detection_name, high_H)
-# def on_low_S_thresh_trackbar(val):
-#     global low_S
-#     global high_S
-#     low_S = val
-#     low_S = min(high_S-1, low_S)
-#     cv.setTrackbarPos(low_S_name, window_detection_name, low_S)
-# def on_high_S_thresh_trackbar(val):
-#     global low_S
-#     global high_S
-#     high_S = val
-#     high_S = max(high_S, low_S+1)
-#     cv.setTrackbarPos(high_S_name, window_detection_name, high_S)
-# def on_low_V_thresh_trackbar(val):
-#     global low_V
-#     global high_V
-#     low_V = val
-#     low_V = min(high_V-1, low_V)
-#     cv.setTrackbarPos(low_V_name, window_detection_name, low_V)
-# def on_high_V_thresh_trackbar(val):
-#     global low_V
-#     global high_V
-#     high_V = val
-#     high_V = max(high_V, low_V+1)
-#     cv.setTrackbarPos(high_V_name, window_detection_name, high_V)
-
 parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
 parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
 
 cv2.namedWindow(window_capture_name)
 cv2.namedWindow(window_setting_name)
-# cv.createTrackbar(rescale_name, window_setting_name, low_rescale, max_rescale, rescale_trackbar)
 cv2.createTrackbar(x0_name, window_setting_name, low_x0, max_x0, x0_trackbar)
 cv2.createTrackbar(y0_name, window_setting_name, low_y0, max_y0, y0_trackbar)
 cv2.createTrackbar(dx_name, window_setting_name, low_dx, max_dx, dx_trackbar)
 cv2.createTrackbar(dy_name, window_setting_name, low_dy, max_dy, dy_trackbar)
 
-
-# cv.createTrackbar(low_H_name, window_detection_name , low_H, max_value_H, on_low_H_thresh_trackbar)
-# cv.createTrackbar(high_H_name, window_detection_name , high_H, max_value_H, on_high_H_thresh_trackbar)
-# cv.createTrackbar(low_S_name, window_detection_name , low_S, max_value, on_low_S_thresh_trackbar)
-# cv.createTrackbar(high_S_name, window_detection_name , high_S, max_value, on_high_S_thresh_trackbar)
-# cv.createTrackbar(low_V_name, window_detection_name , low_V, max_value, on_low_V_thresh_trackbar)
-# cv.createTrackbar(high_V_name, window_detection_name , high_V, max_value, on_high_V_thresh_trackbar)
-
 cap = cv2.VideoCapture(0, cv2.CAP_V4L)
+# cap = cv2.VideoCapture(0)
 cap.set(3, int(1080 / rescale))
 cap.set(4, int(1920 / rescale))
 
 while True:
-    
     m = find_marker.Matching(
         N_=8, 
         M_=14, 
